cfd7bff6d02bfc85cfda4c3575ee93493284f5f1
Hansin Susatya <hansinsusatya@gmail.com>
2020-03-11 15:49:12 +0700

    Add Class Error Boundary

--- sudoku/App.js
+++ sudoku/App.js
@@ -6,19 +6,22 @@ import Home from './src/screens/Home'
 import Board from './src/screens/Game'
 import { BoardProvider } from './src/context/BoardContext'
 import Finish from './src/screens/Finish'
+import ErrorBoundary from './src/components/ErrorBoundary'
 
 export default function App () {
   const Stack = createStackNavigator()
   return (
-    <BoardProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" options={styles} component={Home} />
-          <Stack.Screen name="Game" options={styles} component={Board} />
-          <Stack.Screen name="Result" options={styles} component={Finish} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </BoardProvider>
+    <ErrorBoundary>
+      <BoardProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" options={styles} component={Home} />
+            <Stack.Screen name="Game" options={styles} component={Board} />
+            <Stack.Screen name="Result" options={styles} component={Finish} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </BoardProvider>
+    </ErrorBoundary>
   )
 }
 
